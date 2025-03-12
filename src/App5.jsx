f2145dbb756015b038faa9b80c6c2f0424e3df23
9-Sujal <sujalghorse9@outlook.com>
2025-03-12 07:42:24 +0530

    prop , use context

---
+++ src/App5.jsx
@@ -0,0 +1,66 @@
+import { useContext, useState } from "react";
+import { CountContext } from "./context";
+
+function App5(){
+     const [count, setCount] = useState(0);
+    return(
+        <>
+        <div>
+            <CountContext.Provider value={count}>
+
+            <Count count={count} setCount={setCount}/>
+            </CountContext.Provider>
+            
+        </div>
+        
+        
+        </>
+    )
+}
+
+// eslint-disable-next-line react/prop-types
+function Count({setCount}){
+    return <>
+    <div>
+        <CountRenderer />
+        <Buttons setCount={setCount}/>
+    </div>
+    </>
+}
+
+function CountRenderer(){
+  const count = useContext(CountContext);
+    return<>
+     <div>
+        {count}
+     </div>
+    </>
+}
+
+// eslint-disable-next-line react/prop-types
+function Buttons({setCount}){
+    const count = useContext(CountContext);
+ return<div>
+
+    <button onClick={()=>{
+        setCount(count+1)
+    }}>
+        increase
+    </button>
+  
+
+  
+
+  <button onClick={()=>{
+        setCount(count-1)
+    }}>
+        decrease
+    </button>
+
+ </div>
+}
+
+
+
+
+export default App5; 
